772be89f182f4a56a5bd0875c30c0aa71cdcce1d
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2025-03-18 22:12:46 -0500

    Menu Items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  MenuItems({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+var appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Botones',
+    subTitle: 'Botones de diferentes estilos',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+
+  MenuItems(
+    title: 'Tarjeta',
+    subTitle: 'Un contenedor estilizado',
+    link: '/cards',
+    icon: Icons.credit_card,
+  ),
+];
