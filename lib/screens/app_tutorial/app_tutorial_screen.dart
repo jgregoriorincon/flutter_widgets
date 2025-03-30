ff5b8e41751a522ac5a35ae81a3c3389e3240220
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2025-03-29 21:32:12 -0500

    Add images to tutorial view

--- lib/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SlideInfo {
   final String title;
@@ -17,19 +18,19 @@ final slides = <SlideInfo>[
     title: 'Busca la comida',
     caption:
         'Esse officia nostrud exercitation id anim culpa ut ad incididunt enim dolore aliqua.',
-    imageUrl: '/assets/Images/1.png',
+    imageUrl: 'assets/Images/1.png',
   ),
   const SlideInfo(
     title: 'Entrega rápida',
     caption:
         'Ad culpa tempor deserunt cupidatat culpa ut aliquip dolor eiusmod ad sint sint nisi est.',
-    imageUrl: '/assets/Images/2.png',
+    imageUrl: 'assets/images/2.png',
   ),
   const SlideInfo(
     title: 'Disfruta la comida',
     caption:
         'Laboris voluptate labore eiusmod cupidatat deserunt adipisicing ullamco quis reprehenderit cupidatat elit.',
-    imageUrl: '/assets/Images/3.png',
+    imageUrl: 'assets/images/3.png',
   ),
 ];
 
@@ -41,19 +42,33 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('App Tutorial')),
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children:
-            slides
-                .map(
-                  (slideData) => _Slide(
-                    title: slideData.title,
-                    caption: slideData.caption,
-                    imageUrl: slideData.imageUrl,
-                  ),
-                )
-                .toList(),
+      backgroundColor: Colors.white,
+      // appBar: AppBar(title: const Text('App Tutorial')),
+      body: Stack(
+        children: [
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            children:
+                slides
+                    .map(
+                      (slideData) => _Slide(
+                        title: slideData.title,
+                        caption: slideData.caption,
+                        imageUrl: slideData.imageUrl,
+                      ),
+                    )
+                    .toList(),
+          ),
+
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              child: Text('Salir'),
+              onPressed: () => context.pop(),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -72,6 +87,24 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            const SizedBox(height: 20),
+            Text(title, style: titleStyle),
+            const SizedBox(height: 10),
+            Text(caption, textAlign: TextAlign.center, style: captionStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
